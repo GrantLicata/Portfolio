32d9ce9fdc328a40a114e069658a68f91efa22ad
GrantLicata <grant.licata@gmail.com>
2023-06-03 16:30:54 -0700

    Exploring loading and error pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>©2023 Grant Licata, All rights reserved.</div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
