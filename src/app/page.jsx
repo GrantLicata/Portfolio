0b9c45e5982d2d2a8be4b69b1acc561f56f08819
GrantLicata <grant.licata@gmail.com>
2023-06-06 10:59:55 -0700

    Staging the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,20 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.description}>
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src="/hero.png" width={500} height={500} alt="hero" />
+      </div>
+    </div>
   );
 }
