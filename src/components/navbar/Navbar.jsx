0a58b4169221b72be437254ee0ec2975e57f9db0
GrantLicata <grant.licata@gmail.com>
2023-06-05 17:44:26 -0700

    Navbar and footer styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import styles from "./navbar.module.css";
 
 const links = [
   {
@@ -37,14 +38,24 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">Grant</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Grant
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log("Logged out");
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
