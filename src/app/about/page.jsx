689c362f6effe893f743ccf1f9a37026f1963de0
GrantLicata <grant.licata@gmail.com>
2023-06-10 06:50:54 -0700

    Image source added to config

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const About = () => {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          fill={true}
+          alt=""
+          className={styles.img}
+        ></Image>
+      </div>
+      <div className={styles.textContainer}></div>
+    </div>
+  );
 };
 
 export default About;
