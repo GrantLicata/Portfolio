e7a5f7c58b4e75abb54acdc05586b4f640ef69b8
GrantLicata <grant.licata@gmail.com>
2023-06-10 21:44:00 -0700

    General portfolio page prepared

---
+++ src/app/portfolio/layout.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+import styles from "./page.module.css";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <h1 className={styles.mainTitle}>Our Works</h1>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
