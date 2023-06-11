a9c36cbede39f8ac5a946acaf9813c8045b5d83d
GrantLicata <grant.licata@gmail.com>
2023-06-10 17:43:07 -0700

    About page built

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 const About = () => {
   return (
@@ -11,9 +12,42 @@ const About = () => {
           fill={true}
           alt=""
           className={styles.img}
-        ></Image>
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
       </div>
-      <div className={styles.textContainer}></div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+            corporis consequatur molestiae delectus blanditiis quas eligendi in
+            iste! Vero ratione facilis quaerat quibusdam dolores qui excepturi
+            quam inventore numquam laboriosam!
+            <br />
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+            corporis consequatur molestiae delectus blanditiis quas eligendi in
+            iste! Vero ratione facilis quaerat quibusdam dolores qui excepturi
+            quam inventore numquam laboriosam!
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            sapiente, ipsa dolore officia pariatur aliquam id nobis odio, minus
+            explicabo, dicta dolorem voluptates ducimus illo molestiae molestias
+            iste culpa doloremque.
+            <br />
+            <br /> - Dynamic Websites
+          </p>
+          <Button url="/contact" text="Contact"></Button>
+        </div>
+      </div>
     </div>
   );
 };
