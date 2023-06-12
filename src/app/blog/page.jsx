0e9e9dc8dce71fd9086fecb39b1c7dfeda182e36
GrantLicata <grant.licata@gmail.com>
2023-06-11 18:19:37 -0700

    Portfolio category page prepared

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}>In development</div>;
+  return <div className={styles.container}></div>;
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.categoryTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.description}>Description</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
