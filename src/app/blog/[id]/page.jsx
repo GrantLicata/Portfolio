564462511eb00149cc039ede245358c9a9229442
GrantLicata <grant.licata@gmail.com>
2023-06-11 18:56:21 -0700

    Blog post template prepared;

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import styles from "./page.module.css";
 import React from "react";
 
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/17131288/pexels-photo-17131288/free-photo-of-antelope-canyon-paths.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/17131288/pexels-photo-17131288/free-photo-of-antelope-canyon-paths.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          voluptas, necessitatibus quas temporibus quia veniam maiores
+          voluptatem enim, corporis, deserunt aperiam vitae ullam ipsum aliquid
+          modi repellendus vero sunt nesciunt.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Blog = () => {
   return (
     <div className={styles.mainContainer}>
-      <Link href="/textId" className={styles.container}>
+      <Link href="/blog/textId" className={styles.container}>
         <div className={styles.imageContainer}>
           <Image
             src="https://images.pexels.com/photos/17131288/pexels-photo-17131288/free-photo-of-antelope-canyon-paths.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
