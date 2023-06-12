c8d92086c91bb35877dc3940f3b15f01ac4a6a88
GrantLicata <grant.licata@gmail.com>
2023-06-11 18:20:12 -0700

    Blog page initialized

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,25 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/textId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src=""
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}></h1>
+          <p className={styles.desc}></p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
