405d241b24554b7bc3ee6a154ab2effd20dacdcf
GrantLicata <grant.licata@gmail.com>
2023-06-12 21:09:20 -0700

    Color toggle functionality complete

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import styles from "./DarkModeToggle.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const DarkModeToggle = () => {
-  const mode = "dark";
+  const { toggle, mode } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
