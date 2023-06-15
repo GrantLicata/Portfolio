aad465944f3e36245e70cafddc80dfa89842cf1f
GrantLicata <grant.licata@gmail.com>
2023-06-15 11:31:06 -0700

    User and Post models developed

---
+++ models/Post.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const { schema } = mongoose;
+
+const postScheme = new schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", userSchema);
