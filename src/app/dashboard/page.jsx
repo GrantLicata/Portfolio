443f766c70a797857c1eee254f462036c1018a10
GrantLicata <grant.licata@gmail.com>
2023-06-14 19:44:11 -0700

    useEffect replaced with useSWR

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,30 +2,38 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
 
-      if (!res.ok) {
-        setError(true);
-      }
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      setData(data);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
+  // SWR hook Documentation: https://swr.vercel.app/docs/getting-started
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
   console.log(data);
 
