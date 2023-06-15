df7b20be60a48f3ae900b472e908416fc06ce74d
GrantLicata <grant.licata@gmail.com>
2023-06-14 19:36:39 -0700

    Data gathering for dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        setError(true);
+      }
+
+      const data = await res.json();
+
+      setData(data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  console.log(data);
+
   return <div className={styles.container}>In development</div>;
 };
 
