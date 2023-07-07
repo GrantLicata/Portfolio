2aec17fbce0d4af34b4833ef503f22c9f382dc43
GrantLicata <grant.licata@gmail.com>
2023-07-07 09:04:55 -0700

    Contact manager was added to my portfolio

--- src/app/portfolio/data.js
+++ src/app/portfolio/data.js
@@ -1,5 +1,6 @@
 import DuoChat from "./images/duochat.png";
 import CookBook from "./images/cookbook.jpg";
+import Contact from "./images/contact-manager.png";
 
 export const items = {
   applications: [
@@ -23,6 +24,15 @@ export const items = {
     },
     {
       id: 3,
+      title: "Contact Manager",
+      desc: "Contact Manager was built as a testing ground for learning serverless design with MongoDB and Next.js's latest API management features. It allows any user to enter their name, email, and a message to a list. Each item in that list can then be edited or deleted.",
+      image: Contact,
+      url: "https://contact-manager-gold.vercel.app/",
+      gitHub: "https://github.com/GrantLicata/Contacts",
+      live: true,
+    },
+    {
+      id: 4,
       title: "Scribe",
       desc: "An application built for the student and business professional who needs an open canvas approach to note taking. Users no longer have to separate notes into closed files and folders. All notes are available as an ongoing script to be added to, edited, and deleted as needed.",
       image:
