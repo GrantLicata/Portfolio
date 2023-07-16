f45d05530f2fb00a78dd05db43c8a11c84ef5a02
GrantLicata <grant.licata@gmail.com>
2023-07-16 10:54:46 -0700

    Style updates to experience page

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -21,22 +21,48 @@ const Experience = () => {
     <div className={styles.container}>
       <h1 className={styles.mainTitle}>My Experience</h1>
       <h2 className={styles.categoryTitle}>Employment</h2>
+      <div className={styles.list}>
+        {data.map((item) => (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.cardLeft}>
+              <p className={styles.dates}>
+                {item.year_start} - {item.year_end}
+              </p>
+            </div>
+            <div className={styles.cardRight}>
+              <p className={styles.titleAndEmployer}>
+                {item.title} &bull;{" "}
+                <Link className={styles.link} href={item.employer_url}>
+                  {item.employer}
+                </Link>
+              </p>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
 
-      {data.map((item) => (
-        <div className={styles.item} key={item.id}>
-          <div className={styles.cardLeft}>
-            <p className={styles.dates}>
-              {item.year_start} - {item.year_end}
-            </p>
+      <h2 className={styles.categoryTitle}>Education</h2>
+      <div className={styles.list}>
+        {data.map((item) => (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.cardLeft}>
+              <p className={styles.dates}>
+                {item.year_start} - {item.year_end}
+              </p>
+            </div>
+            <div className={styles.cardRight}>
+              <p className={styles.titleAndEmployer}>
+                {item.title} &bull;{" "}
+                <Link className={styles.link} href={item.employer_url}>
+                  {item.employer}
+                </Link>
+              </p>
+              <p>{item.description}</p>
+            </div>
           </div>
-          <div className={styles.cardRight}>
-            <p className={styles.titleAndEmployer}>
-              {item.title} - {item.employer}
-            </p>
-            <p>{item.description}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
