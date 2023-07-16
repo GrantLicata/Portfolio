2566cab4d5f03f8d0550a7a59a33e218fc09c4f1
GrantLicata <grant.licata@gmail.com>
2023-07-15 20:24:14 -0700

    Experience data entered

---
+++ src/app/experience/data.js
@@ -0,0 +1,51 @@
+export const items = {
+  applications: [
+    {
+      title: "Project Manager",
+      employer: "Seagen",
+      year_start: 2021,
+      year_end: "Present",
+      employer_url: "https://www.seagen.com/",
+      description:
+        "Leading project teams towards the successful development and implementation of new programs, equipment, and facilities.",
+      skills: [
+        "Project Management",
+        "Program Development",
+        "Portfolio Management",
+        "Process Improvement",
+        "Waterfall Methodologies",
+      ],
+    },
+    {
+      title: "Quality Assurance Project Manager",
+      employer: "BioLife Solutions",
+      year_start: 2018,
+      year_end: 2021,
+      employer_url: "https://www.biolifesolutions.com/",
+      description:
+        "Managing a diverse portfolio of projects across the organization including new product development, operational excellence, and process improvement.",
+      skills: [
+        "Project Management",
+        "Portfolio Management",
+        "Process Improvement",
+        "Waterfall Methodologies",
+        "Operational Excellence",
+      ],
+    },
+    {
+      title: "Project Manager",
+      employer: "American Freeze Dry",
+      year_start: 2016,
+      year_end: 2018,
+      employer_url: "https://innovativefdf.com/",
+      description:
+        "Led the development and improvement efforts of systems, processes, and products to ensure excellence in right the first time contract manufacturing operations and while expanding the business into new consumer markets.",
+      skills: [
+        "Project Management",
+        "Product Development",
+        "Process Improvement",
+        "Operational Excellence",
+      ],
+    },
+  ],
+};
--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -4,19 +4,51 @@ import styles from "./page.module.css";
 
 const Experience = () => {
   return (
-    <div>
-      <h1>Experience</h1>
-      <p>This page will present my professional experience.</p>
-      <p>
-        The theme will follow a similar format to the one found at the link
-        below...
-      </p>
-      <div className={styles.portfolioLink}>
-        <Link href="https://brittanychiang.com/#experience">
-          Brittany Chiang - Experience
-        </Link>
-      </div>
+    <div className={styles.container}>
+      <h1 className={styles.mainTitle}>My Experience</h1>
+      <h2 className={styles.categoryTitle}>Employment</h2>
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.description}>{item.desc}</p>
+            {item.live ? (
+              <Button text="Visit The App" url={item.url} />
+            ) : (
+              <Link href={item.gitHub}>
+                <button className={styles.inDevelopmentButton}>
+                  In Development
+                </button>
+              </Link>
+            )}
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              sizes="max-width: 598px, max-height: 500px"
+              alt=""
+            />
+          </div>
+        </div>
+      ))}
     </div>
+
+    // <div>
+    //   <h1>Experience</h1>
+    //   <p>This page will present my professional experience.</p>
+    //   <p>
+    //     The theme will follow a similar format to the one found at the link
+    //     below...
+    //   </p>
+    //   <div className={styles.portfolioLink}>
+    //     <Link href="https://brittanychiang.com/#experience">
+    //       Brittany Chiang - Experience
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
 
