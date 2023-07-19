f99010c14187ddfdbc7d26b55340b5859c83f871
GrantLicata <grant.licata@gmail.com>
2023-07-18 20:55:17 -0700

    Experience page updated for center line

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -42,102 +42,108 @@ const Experience = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.mainTitle}>My Experience</h1>
-      <h2 className={styles.categoryTitle}>Employment</h2>
-      <div className={styles.list}>
-        {employment.map((item) => (
-          <div
-            className={mode === "dark" ? styles.item : styles.lightModeItem}
-            key={item.id}
-          >
-            <div className={styles.cardLeft}>
-              <p className={styles.dates}>
-                {item.year_start} - {item.year_end}
-              </p>
-            </div>
-            <div className={styles.cardRight}>
-              <p
-                className={
-                  mode === "dark" ? styles.cardTitle : styles.lightModeCardTitle
-                }
-              >
-                {item.title} &bull;{" "}
-                <Link
+      <div className={styles.experience}>
+        <h2 className={styles.categoryTitle}>Employment</h2>
+        <div className={styles.list}>
+          {employment.map((item) => (
+            <div
+              className={mode === "dark" ? styles.item : styles.lightModeItem}
+              key={item.id}
+            >
+              <div className={styles.cardLeft}>
+                <p className={styles.dates}>
+                  {item.year_start} - {item.year_end}
+                </p>
+              </div>
+              <div className={styles.cardRight}>
+                <p
                   className={
-                    mode === "dark" ? styles.link : styles.lightModeLink
+                    mode === "dark"
+                      ? styles.cardTitle
+                      : styles.lightModeCardTitle
                   }
-                  href={item.employer_url}
                 >
-                  {item.employer}
-                </Link>
-              </p>
-              <p>{item.description}</p>
+                  {item.title} &bull;{" "}
+                  <Link
+                    className={
+                      mode === "dark" ? styles.link : styles.lightModeLink
+                    }
+                    href={item.employer_url}
+                  >
+                    {item.employer}
+                  </Link>
+                </p>
+                <p>{item.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      <h2 className={styles.categoryTitle}>Education</h2>
-      <div className={styles.list}>
-        {education.map((item) => (
-          <div
-            className={mode === "dark" ? styles.item : styles.lightModeItem}
-            key={item.id}
-          >
-            <div className={styles.cardLeft}>
-              <p className={styles.dates}>
-                {item.year_start} - {item.year_end}
-              </p>
-            </div>
-            <div className={styles.cardRight}>
-              <p
-                className={
-                  mode === "dark" ? styles.cardTitle : styles.lightModeCardTitle
-                }
-              >
-                {item.degree} &bull;{" "}
-                <Link
+        <h2 className={styles.categoryTitle}>Education</h2>
+        <div className={styles.list}>
+          {education.map((item) => (
+            <div
+              className={mode === "dark" ? styles.item : styles.lightModeItem}
+              key={item.id}
+            >
+              <div className={styles.cardLeft}>
+                <p className={styles.dates}>
+                  {item.year_start} - {item.year_end}
+                </p>
+              </div>
+              <div className={styles.cardRight}>
+                <p
                   className={
-                    mode === "dark" ? styles.link : styles.lightModeLink
+                    mode === "dark"
+                      ? styles.cardTitle
+                      : styles.lightModeCardTitle
                   }
-                  href={item.school_url}
                 >
-                  {item.school}
-                </Link>
-              </p>
-              <p>{item.description}</p>
+                  {item.degree} &bull;{" "}
+                  <Link
+                    className={
+                      mode === "dark" ? styles.link : styles.lightModeLink
+                    }
+                    href={item.school_url}
+                  >
+                    {item.school}
+                  </Link>
+                </p>
+                <p>{item.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      <h2 className={styles.categoryTitle}>Certifications</h2>
-      <div className={styles.list}>
-        {certifications.map((item) => (
-          <div
-            className={mode === "dark" ? styles.item : styles.lightModeItem}
-            key={item.id}
-          >
-            <div className={styles.cardLeft}>
-              <p className={styles.dates}>
-                {item.year_start} - {item.year_end}
-              </p>
+        <h2 className={styles.categoryTitle}>Certifications</h2>
+        <div className={styles.list}>
+          {certifications.map((item) => (
+            <div
+              className={mode === "dark" ? styles.item : styles.lightModeItem}
+              key={item.id}
+            >
+              <div className={styles.cardLeft}>
+                <p className={styles.dates}>
+                  {item.year_start} - {item.year_end}
+                </p>
+              </div>
+              <div className={styles.cardRight}>
+                <p className={styles.titleAndEmployer}>
+                  {item.title} &bull;{" "}
+                  <Link
+                    className={
+                      mode === "dark" ? styles.link : styles.lightModeLink
+                    }
+                    href={item.organization_url}
+                  >
+                    {item.organization}
+                  </Link>
+                </p>
+                <p>{item.description}</p>
+              </div>
             </div>
-            <div className={styles.cardRight}>
-              <p className={styles.titleAndEmployer}>
-                {item.title} &bull;{" "}
-                <Link
-                  className={
-                    mode === "dark" ? styles.link : styles.lightModeLink
-                  }
-                  href={item.organization_url}
-                >
-                  {item.organization}
-                </Link>
-              </p>
-              <p>{item.description}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
